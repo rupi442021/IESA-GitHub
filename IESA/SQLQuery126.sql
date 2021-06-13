c94dcb011a8315cb823cb6121628dafbda075255
Shay David <73243684+Shax125@users.noreply.github.com>
2021-06-13 23:31:31 +0300

    border acitions

--- IESA/SQLQuery126.sql
+++
@@ -1,1 +0,0 @@
-﻿select * from Competitions where competitionstatus = 2
--- IESA/SQLQuery166.sql
+++
@@ -1,9 +0,0 @@
-﻿select * from Gamer_Competition
-
-UPDATE Competitions
-SET competitiontype = 2
-WHERE isonline = 0;
-
-UPDATE Gamer_Competition
-SET status1 = 0
-WHERE competitionid = 19;
