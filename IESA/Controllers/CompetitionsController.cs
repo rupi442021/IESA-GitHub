e9c5387717c09d91139bd0ae526c58f7651c3a14
BetzalelGRP <77381335+BetzalelGRP@users.noreply.github.com>
2021-07-22 19:47:48 +0300

    Responsive Site Update 22/07

--- IESA/Controllers/CompetitionsController.cs
+++ IESA/Controllers/CompetitionsController.cs
@@ -2,9 +2,12 @@
 using IESA.Models.DAL;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
+using System.Web;
 using System.Web.Http;
 
 namespace IESA.Controllers
